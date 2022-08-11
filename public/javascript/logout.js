63ff633fd49c3df2cebc52f35e2387abe9ed1471
ThereWolf <bdunham484@gmail.com>
2022-08-11 13:18:10 -0500

    add alert

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -9,7 +9,7 @@ async function logout() {
     if (response.ok) {
         document.location.replace('/');
     } else {
-        alert(response.statusText);
+        alert("You're not logged in!.");
     }
 }
 
