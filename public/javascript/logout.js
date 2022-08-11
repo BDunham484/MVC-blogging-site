9a1ac41a5da3f89f0dba93ff23bb0bb9cf31f534
ThereWolf <bdunham484@gmail.com>
2022-08-11 13:30:38 -0500

    function test

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,10 +1,11 @@
 //function that runs when #logout button is clikced in main.handlebars
 //posts via user.routes.js
 async function logout() {
-    const response = await fetch('/api/users/logout', {
-        method: 'post',
-        headers: { 'Content-Type': 'application/json' }
-    });
+        const response = await fetch('/api/users/logout', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' }
+        });
+    
 
     if (response.ok) {
         document.location.replace('/');
